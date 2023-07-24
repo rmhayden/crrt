2c0c3bf36be89c1bb1bd2c06bf1820b4d409c592
rmhayden <robertmorsehayden@gmail.com>
2023-07-24 12:46:05 -0400

    now have functions to add and remove toxins

--- script.js
+++ script.js
@@ -4,11 +4,13 @@
 /*----- CONSTANTS -----*/
 
 
+
+
 /*----- STATE VARIABLES -----*/
 
 
 let currentLabValues = {
-    potassium: 5,
+    potassium: 7,
     bicarb: 20
 }
 
@@ -16,6 +18,14 @@ let allLabValues = {
     potassium: [],
     bicarb: []
 }
+
+let potassiumClearanceValue = 2
+// will later model this accurately; for now, will score 1-3 based on Rx
+// for now, will just set 4 as lower limit (or zero, if zeroK)
+
+let bicarbClearanceValue = 2
+// will later model this accurately; for now, will score 1-3 based on Rx
+
 
 /*----- CACHED ELEMENTS  -----*/
 
@@ -34,13 +44,19 @@ const allLabValueEls = {
 
 
 function init() {
-    
-    appendNewLabValues()
+
+    interval = 10000 // one minute is a cycle ### To do: make timer animation
+    timer = setInterval(continueGame, interval)
+        // mirrored the function we used in the Tamagotchi game
+    cycles = 0 // starts at zero
+
+    render()
+    continueGame()
 }
 
 
 function render() {
-
+    // this may be best included for DOM only? ###
     appendNewLabValues()
 }
 
@@ -52,4 +68,59 @@ function appendNewLabValues() {
         }   
     }
 
+
+function continueGame(){
+    // mirrored the function we used in the Tamagotchi game
+    let lastCycle = cycles
+    cycles++ //so game cycles increase with each 'runGame'
+    console.log(`Current Cycle: ${cycles}`)
+
+    if (lastCycle < cycles) {
+    nextInterval()
+    console.log(lastCycle)
+    }
+}
+
+function nextInterval() {
+    // either time or button event triggers next interval
+    toxinAccumulation()
+    toxinClearance()
+    console.log("Next Interval")
+}
+
+
+function gameOver() {
+
+}
+
+
+function toxinAccumulation() {
+    currentLabValues.potassium += 1
+    currentLabValues.bicarb -= 4
+    console.log(currentLabValues)
+}
+
+function toxinClearance() {
+    potassiumClearance()
+    bicarbClearance()
+}
+
+
+function potassiumClearance () {
+ // this will be run after user clicks "sign orders" button
+    if (currentLabValues.potassium > 4 && potassiumClearanceValue === 1) {
+        currentLabValues.potassium -= 1
+    } else if (currentLabValues.potassium > 4 && potassiumClearanceValue === 2) {
+        currentLabValues.potassium -= 2
+    }
+}
+
+function bicarbClearance () {
+    // this will be run after user clicks "sign orders" button
+       if (currentLabValues.bicarb < 24 && bicarbClearanceValue === 1) {
+            currentLabValues.bicarb += 4
+       } else if (currentLabValues.bicarb < 24 && bicarbClearanceValue === 2) {
+            currentLabValues.bicarb += 8
+       }
+   }
 
