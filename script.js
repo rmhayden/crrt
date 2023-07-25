a3d3d8ece9487ce682520219d1b7dbef5f7e7a85
rmhayden <robertmorsehayden@gmail.com>
2023-07-24 20:18:09 -0400

    now have potassiums added

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ let currentLabValues = {
     bicarb: 20
 }
 
+//to do ### for let to round this^ ?
+
+
 let allLabValues = {
     potassium: [],
     bicarb: []
 }
-
-
 
 
 let potassiumClearanceValue = 2
@@ -36,15 +37,24 @@ let bicarbClearanceValue = 2
 const potassiumEl = document.querySelector('.potassium')
 const bicarbEl  = document.querySelector('.bicarb')
 
-// ### need to cach the RFR value!
+
+const potassiumEl1 = document.querySelector('.potassium1')
+const potassiumEl2 = document.querySelector('.potassium2')
+const potassiumEl3 = document.querySelector('.potassium3')
+const potassiumEl4 = document.querySelector('.potassium4')
+const potassiumEl5 = document.querySelector('.potassium5')
+const potassiumEl6 = document.querySelector('.potassium6')
+
+let replacementFluidRateValue
 
 const startBtnEl = document.querySelector('.start')
 const updateOrderBtnEl = document.querySelector('.update-order-button')
 
+
 /*----- EVENT LISTENERS -----*/
 
 startBtnEl.addEventListener('click', handleStartClick)
-updateOrderBtnEl.addEventListener('click', displayRFRValue)
+updateOrderBtnEl.addEventListener('click', updateRFRValue)
 
 
 /*----- FUNCTIONS -----*/
@@ -57,7 +67,7 @@ function handleStartClick() {
 
 function init() {
 
-    interval = 10000 // one minute is a cycle ### To do: make timer animation
+    interval = 2000 // one minute is a cycle ### To do: make timer animation
     timer = setInterval(continueGame, interval)
         // mirrored the function we used in the Tamagotchi game
     cycles = 0 // starts at zero
@@ -77,16 +87,47 @@ function render() {
 function appendNewLabValues() {
     for (let key in allLabValues) {
         allLabValues[key].push((`&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${currentLabValues[key]}`))
-        console.log(`${allLabValues[key]}    `)
+        // console.log(`${allLabValues[key]}`)
         }   
     }
 
 function renderAllLabValues() {
 
-    potassiumEl.innerHTML = `Potassium: ${allLabValues.potassium}`
-    bicarbEl.innerHTML = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bicarb: ${allLabValues.bicarb}`
+        potassiumEl.innerHTML = `&nbsp;${allLabValues.potassium}`
+        bicarbEl.innerHTML = `${allLabValues.bicarb}`
 
+    
+
+    if (cycles === 0) {
+        potassiumEl1.innerHTML = `${allLabValues.potassium[0]}`
+    } else if (cycles === 1) {
+        potassiumEl1.innerHTML = `${allLabValues.potassium[0]}`
+        potassiumEl2.innerHTML = `${allLabValues.potassium[1]}`
+    } else if (cycles === 2) {
+        potassiumEl1.innerHTML = `${allLabValues.potassium[0]}`
+        potassiumEl2.innerHTML = `${allLabValues.potassium[1]}`
+        potassiumEl3.innerHTML = `${allLabValues.potassium[2]}`
+    } else if (cycles === 3) {
+        potassiumEl1.innerHTML = `${allLabValues.potassium[0]}`
+        potassiumEl2.innerHTML = `${allLabValues.potassium[1]}`
+        potassiumEl3.innerHTML = `${allLabValues.potassium[2]}`
+        potassiumEl4.innerHTML = `${allLabValues.potassium[3]}`
+    } else if (cycles === 4) {
+        potassiumEl1.innerHTML = `${allLabValues.potassium[0]}`
+        potassiumEl2.innerHTML = `${allLabValues.potassium[1]}`
+        potassiumEl3.innerHTML = `${allLabValues.potassium[2]}`
+        potassiumEl4.innerHTML = `${allLabValues.potassium[3]}`
+        potassiumEl5.innerHTML = `${allLabValues.potassium[4]}`
+    } else if (cycles === 5) {
+        potassiumEl1.innerHTML = `${allLabValues.potassium[0]}`
+        potassiumEl2.innerHTML = `${allLabValues.potassium[1]}`
+        potassiumEl3.innerHTML = `${allLabValues.potassium[2]}`
+        potassiumEl4.innerHTML = `${allLabValues.potassium[3]}`
+        potassiumEl5.innerHTML = `${allLabValues.potassium[4]}`
+        potassiumEl6.innerHTML = `${allLabValues.potassium[5]}`
+    } // perhaps if made this an object could use iterative process to make drier
 }
+
 
 function continueGame(){
     // mirrored the function we used in the Tamagotchi game
@@ -95,9 +136,17 @@ function continueGame(){
     console.log(`Current Cycle: ${cycles}`)
 
     if (lastCycle < cycles) {
-    nextInterval()
+    // nextInterval()
+    goToNextOrEndGame()
     console.log(lastCycle)
     }
+}
+
+function goToNextOrEndGame() {
+    if (cycles <= 5) {
+        nextInterval()
+    } else gameOver()
+
 }
 
 function nextInterval() {
@@ -110,7 +159,7 @@ function nextInterval() {
 
 
 function gameOver() {
-
+  console.log("Game Over!")
 }
 
 
@@ -134,6 +183,17 @@ function potassiumClearance() {
     }
 }
 
+function potassiumClearanceValueModulation() {
+    if (replacementFluidRateValue = 0) {
+        potassiumClearanceValue = 0
+    } else if (replacementFluidRateValue = 1600) {
+        potassiumClearanceValue = 1
+    } else if (replacementFluidRateValue = 2400) {
+    potassiumClearanceValue = 2
+    } else if (replacementFluidRateValue = 3200) {
+        potassiumClearanceValue = 3}
+} // ### this needs to be activated
+
 function bicarbClearance() {
     // this will be run after user clicks "sign orders" button
        if (currentLabValues.bicarb < 24 && bicarbClearanceValue === 1) {
@@ -141,20 +201,24 @@ function bicarbClearance() {
        } else if (currentLabValues.bicarb < 24 && bicarbClearanceValue === 2) {
             currentLabValues.bicarb += 8
        }
-   }
+}
 
 
-    function displayRFRValue() {
-        // SRC: https://www.geeksforgeeks.org/how-to-get-value-of-selected-radio-button-using-javascript/
-        document.getElementById("replacement-fluid-rate").innerHTML = "";
-        let ele = document.getElementsByTagName('input');
-        for (i = 0; i < ele.length; i++) {
-            if (ele[i].type = "radio") {
-                if (ele[i].checked) 
-                    document.getElementById("replacement-fluid-rate").innerHTML
-                        += ele[i].name + " Value: "
-                        + ele[i].value + "<br>";
-            }
+function updateRFRValue () {
+  // SRC: https://www.geeksforgeeks.org/how-to-get-value-of-selected-radio-button-using-javascript/
+    document.getElementById("replacement-fluid-rate").innerHTML = "";
+    let el = document.getElementsByTagName('input');
+    for (i = 0; i < el.length; i++) {
+        if (el[i].checked) {
+            replacementFluidRateValue = el[i].value
+            // console.log(replacementFluidRateValue)
+            document.getElementById("replacement-fluid-rate").innerHTML = `Replacement Fluid Rate: ${replacementFluidRateValue}`
         }
-    }
+    } 
+}
+
+
+function round (num) {
+    return num.toFixed(1)
+} // may need this later to round the lab values
 
